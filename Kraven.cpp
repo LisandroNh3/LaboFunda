8a06538c87d7590d35170a560447d7424ba70c09
LisandroNh3 <00025825@uca.edu.sv>
2025-04-28 14:47:51 -0600

    Create Kraven.cpp

---
+++ Kraven.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    string quesoy = "";
+
+// Describiendo que es el sujeto
+    cout << "Que eres papu (malo, animal, persona)";
+    cin >> quesoy;
+
+    //Dando diferentes finales por determinante del sujeto
+    if (quesoy == "animal")
+    {
+        cout << "Kraven te apapacha y te protege:D";
+    }
+    else if (quesoy == "persona")
+    {
+        cout << "Kraven te saluda amablemente :D";
+    }
+
+    else if (quesoy == "malo")
+    {
+        cout << "Ni le reses al de arriba pq ese es Kraven D:";
+    }
+}
